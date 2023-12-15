722c7079750247be235a280419a23211cde5a888
fatih <fatihtutar93@gmail.com>
2023-12-15 11:03:39 +0300

    config.php edited

--- config.php
+++ config.php
@@ -5,3 +5,8 @@ const URL = 'http://localhost:8888/Patika/cms-oop-php/';
 // Windows URL
 //const URL = 'http://localhost/patika/cms-oop-php/';
 const BASEDIR = __DIR__;
+
+const HOST = 'localhost';
+const DB = 'patika';
+const DB_USER = 'root';
+const DB_PASSWORD = 'root';
